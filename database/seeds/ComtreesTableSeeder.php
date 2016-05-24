e2e9193184e7eaf1e1d95e054a6ed910797ad82b
speaker73 <speaker73@ukr.net>
2016-05-24 23:09:18 +0300

    edit view.

--- database/seeds/ComtreesTableSeeder.php
+++ database/seeds/ComtreesTableSeeder.php
@@ -12,24 +12,28 @@ class ComtreesTableSeeder extends Seeder
     public function run()
     {
         DB::table('comtrees')->insert([
+            'id' => 1,
             'name' => 'Company1',
             'amount' => 25,
             'total_amount' => 53,
             'parent' => 1
         ]);
         DB::table('comtrees')->insert([
+            'id' => 2,
             'name' => 'Company2',
             'amount' => 13,
             'total_amount' => 18,
             'parent' => 1
         ]);
         DB::table('comtrees')->insert([
+            'id' => 3,
             'name' => 'Company3',
             'amount' => 5,
             'total_amount' => 5,
             'parent' => 2
         ]);
         DB::table('comtrees')->insert([
+            'id' => 4,
             'name' => 'Company4',
             'amount' => 10,
             'total_amount' => 10,
