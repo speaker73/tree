fd21a27909f6b518491311c30dfe6f33523b864b
speaker73 <speaker73@ukr.net>
2016-05-25 12:35:14 +0300

    add global bootsrap and jqery link

--- resources/views/comtree.blade.php
+++ resources/views/comtree.blade.php
@@ -4,15 +4,11 @@
     <meta charset="UTF-8">
     <title>Tree</title>
     <!-- Latest compiled and minified CSS -->
-{{--
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
---}}
-    <link rel="stylesheet" href="bower_resources/bootstrap/dist/css/bootstrap.min.css">
+   {{-- <link rel="stylesheet" href="bower_resources/bootstrap/dist/css/bootstrap.min.css">--}}
     <!-- Optional theme -->
-{{--
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
---}}
-    <link rel="stylesheet" href="bower_resources/bootstrap/dist/css/bootstrap-theme.css">
+   {{-- <link rel="stylesheet" href="bower_resources/bootstrap/dist/css/bootstrap-theme.css">--}}
 </head>
 <body>
 
@@ -127,13 +123,11 @@
 @endforeach
 
 <!-- script -->
-{{--<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>--}}
-    <script src="bower_resources/jquery/dist/jquery.min.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+    {{--<script src="bower_resources/jquery/dist/jquery.min.js"></script>--}}
     <!-- Latest compiled and minified JavaScript -->
-{{--
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
---}}
-    <script src="bower_resources/bootstrap/dist/js/bootstrap.min.js"></script>
+    {{--<script src="bower_resources/bootstrap/dist/js/bootstrap.min.js"></script>--}}
     <script type="text/javascript">
 
                 /*Build Tree*/
