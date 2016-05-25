d8b9904c4bfa62b1648d331c60452f1bb7384cf6
speaker73 <speaker73@ukr.net>
2016-05-25 14:29:14 +0300

    delete from arr

--- resources/views/comtree.blade.php
+++ resources/views/comtree.blade.php
@@ -4,11 +4,15 @@
     <meta charset="UTF-8">
     <title>Tree</title>
     <!-- Latest compiled and minified CSS -->
+{{--
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-   {{-- <link rel="stylesheet" href="bower_resources/bootstrap/dist/css/bootstrap.min.css">--}}
+--}}
+    <link rel="stylesheet" href="bower_resources/bootstrap/dist/css/bootstrap.min.css">
     <!-- Optional theme -->
+{{--
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-   {{-- <link rel="stylesheet" href="bower_resources/bootstrap/dist/css/bootstrap-theme.css">--}}
+--}}
+    <link rel="stylesheet" href="bower_resources/bootstrap/dist/css/bootstrap-theme.css">
 </head>
 <body>
 
@@ -123,11 +127,13 @@
 @endforeach
 
 <!-- script -->
-<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-    {{--<script src="bower_resources/jquery/dist/jquery.min.js"></script>--}}
+{{--<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>--}}
+    <script src="bower_resources/jquery/dist/jquery.min.js"></script>
     <!-- Latest compiled and minified JavaScript -->
+{{--
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    {{--<script src="bower_resources/bootstrap/dist/js/bootstrap.min.js"></script>--}}
+--}}
+    <script src="bower_resources/bootstrap/dist/js/bootstrap.min.js"></script>
     <script type="text/javascript">
 
                 /*Build Tree*/
@@ -184,33 +190,7 @@
     }
                 /*The order of addition*/
 
-function order(){
 
-    /* Добавляем елементы у которых есть child, но которые тоже имеют родителя */
-    for(var i=0;i<tree_arr.length;i++){
-        var our_parent = $('#' + tree_arr[i]).attr('parent');
-        var our_parent_length = $('.list-group-item[parent="'+tree_arr[i]+'"]').length;
-        if(our_parent_length > 0 ){
-            if(tree_arr[i]!= our_parent){
-                total_add(tree_arr[i]);
-            }
-
-        }
-    }
-
-    /* добавляем елементы без родителей */
-
-    for(var i2=0;i2<tree_arr.length;i2++){
-        var our_parent2 = $('#' + tree_arr[i2]).attr('parent');
-        var our_parent_length2 = $('.list-group-item[parent="'+tree_arr[i2]+'"]').length;
-        if(our_parent2 == tree_arr[i2] ){
-
-            total_add(tree_arr[i2]);
-
-
-        }
-    }
-}
                 var level_arr = [];
   function order_add_level(){
 
@@ -248,27 +228,26 @@ function order(){
                 function order_calc() {
                     console.log("Level arr: "+level_arr+" Tree arr: "+tree_arr);
                     for(var i = 0;i<tree_arr.length;i++){
-
+                        var level = level_arr[i];
 
                         for(var j = 0;j<level_arr.length;j++){
-                            var level = level_arr[i];
-                        var id_level = Number($('#' + tree_arr[j]).attr('level'));
+                            var id_level = Number($('#' + tree_arr[j]).attr('level'));
                             console.log("level="+level+" id_level="+id_level);
 
-                        if(level===id_level){
-                            console.log(level+" Print it!"+id_level);
-                            total_add(tree_arr[j]);
-                            console.log("delete level_arr: "+i+" value: "+level_arr[i]);
-                            /*level_arr.splice(i, 1);*/
-                            delete level_arr[i];
-                            console.log(level_arr);
+                            if(level==id_level){
+                                console.log(level+" Print it! "+tree_arr[j]);
+                                total_add(tree_arr[j]);
+                               /* console.log("delete level_arr: "+i+" value: "+level_arr[i]);
+                                /!*level_arr.splice(i, 1);*!/
+                                delete level_arr[i];
+                                console.log("level_arr:"+level_arr);*/
 
-                           /* console.log("splice tree_arr: "+j+" value: "+tree_arr[j]);*/
-                          /*  tree_arr.splice(j, 1);*/
-                            console.log("delete tree_arr: "+j+" value: "+tree_arr[i]);
-                            delete tree_arr[j];
-                            console.log(tree_arr);
-                        }
+                                 /*console.log("splice tree_arr: "+j+" value: "+tree_arr[j]);*/
+                                 /* tree_arr.splice(j, 1);*/
+                                /*console.log("delete tree_arr: "+j+" value: "+tree_arr[j]);
+                                delete tree_arr[j];
+                               console.log("tree_arr:"+tree_arr);*/
+                            }
                         }
                     }
 
